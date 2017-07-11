1d870c933ec78d2f80cc053a3a9926784009f173
Mandeep Singh <mandeep6268@gmail.com>
2017-07-11 02:26:26 -0700

    Updated app default path and Changed app title

--- src/app/layout/components/app.view/app.view.component.ts
+++ src/app/layout/components/app.view/app.view.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationStart } from '@angular/router';
-import { AppConfigService } from '../../../wize-ng2-core/core/shared/services/index';
+import { AppConfigService, UIConfigService } from '../../../wize-ng2-core/core/shared/services/index';
 @Component({
   selector: 'app-view',
   templateUrl: './app.view.html',
@@ -9,13 +9,13 @@ import { AppConfigService } from '../../../wize-ng2-core/core/shared/services/in
 
 export class AppViewComponent implements OnInit{
   constructor(private router: Router,
-              private appConfigService: AppConfigService) {
+              private appConfigService: AppConfigService, private uiConfigService: UIConfigService) {
 
   }
 
   ngOnInit() {
     if(this.router.url === '/' || this.router.url === '') {
-      this.router.navigate([this.appConfigService.getConfig('defaultRoute')]);
+      this.router.navigate([this.uiConfigService.getFirstRoute()]);
     }
   }
 }
